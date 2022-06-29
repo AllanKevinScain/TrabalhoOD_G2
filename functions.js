28e3b343e186ed4ae94d8febb5faa7afa28e30a2
Eduardo Meurer Faccat <eduardomeurer@sou.faccat.br>
2022-06-28 21:43:40 -0300

    Upload dos xmls assincrono; Leitura assincrona da fila; Upload assincrono do json;

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@ import { info } from "console";
 
 function criarJSON(nome, data) {
   fs.writeFileSync(path.resolve(PASTA_JSON, nome), data);
+  return { caminho: path.resolve(PASTA_JSON, nome), filename: nome };
 }
 
 function xml2json(data) {
@@ -22,66 +23,43 @@ async function uploadXML() {
   let arrayArquivos = fs.readdirSync(PASTA_XML);
   let blobServiceClient = storageBlob.BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
   let containerClient = blobServiceClient.getContainerClient("contoso-xml");
-  arrayArquivos.forEach(async filename => {
+  for (const filename of arrayArquivos) {
     let caminho = path.resolve(PASTA_XML, filename);
     let infoFile = fs.statSync(caminho);
-
     let blobClient = containerClient.getBlockBlobClient(filename);
     let bufferSize = (1 * 1024 * 1024) / 2;
     let file = fs.createReadStream(caminho);
 
     try {
-      console.log("Enviando arquivo" + filename);
-      await blobClient.uploadStream(file, bufferSize, 5, {
+      blobClient.uploadStream(file, bufferSize, 5, {
         onProgress: (ev) => {
-          let progress = ev.loadedBytes / infoFile.size;
-          console.log(Math.round(progress*100) + "% conluido");
+          console.log("Progresso do upload do arquivo", filename, " ", Math.round((ev.loadedBytes / infoFile.size)*100), "%");
         }
       })
     } catch (e) {
       console.log(e);
     }
-    // blobClient.uploadData(file, {
-    //   onProgress: (ev) => {
-    //     console.log(ev);
-    //   }
-    // })
-
-
-  })
-
-
+  }
 }
 
-async function uploadJSON() {
+async function uploadJSON(stream, filename) {
 
-  let arrayArquivos = fs.readdirSync(PASTA_JSON);
   let blobServiceClient = storageBlob.BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
   let containerClient = blobServiceClient.getContainerClient("contoso-json");
-  arrayArquivos.forEach(async filename => {
-    let caminho = path.resolve(PASTA_JSON, filename);
-    let infoFile = fs.statSync(caminho);
 
-    let blobClient = containerClient.getBlockBlobClient(filename);
-    let bufferSize = (1 * 1024 * 1024) / 2;
-    let file = fs.createReadStream(caminho);
+  let blobClient = containerClient.getBlockBlobClient(filename);
+  let bufferSize = (1 * 1024 * 1024) / 2;
 
-    try {
-      console.log("Enviando arquivo " + filename);
-      await blobClient.uploadStream(file, bufferSize, 5, {
-        onProgress: (ev) => {
-          let progress = ev.loadedBytes / infoFile.size;
-          console.log(Math.round(progress*100) + " conluido",);
-        }
-      })
-      fs.unlinkSync(caminho);
-    } catch (e) {
-      console.log(e);
-    }
-   
-  });
+  try {
+    await blobClient.uploadStream(stream, bufferSize, 5, {
+      onProgress: (ev) => {
+      }
+    })
+  } catch (e) {
+    console.log(e);
+  }
 
-}
+};
 
 function sleep(ms) {
   return new Promise((resolve) => {
--- index.js
+++ index.js
@@ -4,53 +4,68 @@ import { QueueClient } from '@azure/storage-queue';
 import { AZURE_STORAGE_CONNECTION_STRING } from './config.js';
 import { sleep, xml2json, criarJSON, uploadJSON, uploadXML, base642json } from './functions.js';
 import axios from 'axios';
+import fs from 'fs';
 
-function main(initial = 0) {
+let semaforo = 0;
+console.log("Iniciando upload assíncrono dos arquivos xml!");
 
+async function main() {
   while (true) {
-    if (initial == 0) {
-      uploadXML().then(() => {
-        
-        const nomeFila = "contoso-fila";
+    const nomeFila = "contoso-fila";
 
-        // Instantiate a QueueClient which will be used to create and manipulate a queue
-        const queueClient = new QueueClient(
-          AZURE_STORAGE_CONNECTION_STRING,
-          nomeFila
-        );
+    // Instantiate a QueueClient which will be used to create and manipulate a queue
+    const queueClient = new QueueClient(
+      AZURE_STORAGE_CONNECTION_STRING,
+      nomeFila
+    );
+    console.log("Lendo a fila de 1 por 1");
+    let mensagensFila = await queueClient.receiveMessages({ numberOfMessages: 1 });
+    // const peekedMessages = await queueClient.peekMessages();
+    if (mensagensFila.receivedMessageItems.length > 0) {
+      let mensagemRecebida = mensagensFila.receivedMessageItems[0];
+      let jsonData = base642json(mensagemRecebida.messageText);
+      let url = jsonData.data.url;
+      let name = url.substring(url.lastIndexOf('/') + 1);
 
-        queueClient.receiveMessages({ numberOfMessages: 1 }).then((mensagensFila) => {
-          // const peekedMessages = await queueClient.peekMessages();
-          if (mensagensFila.receivedMessageItems.length == 0) {
-            return;
-          }
-          let mensagemRecebida = mensagensFila.receivedMessageItems[0];
-          let jsonData = base642json(mensagemRecebida.messageText);
-          let url = jsonData.data.url;
-          console.log(url);
-          let filename = url.substring(url.lastIndexOf('/') + 1);
-          axios.get(url).then((xml) => {
-            let json = xml2json(xml.data);
-            criarJSON(filename.replace(".xml", ".json"), json);
-            uploadJSON().then(() => {
-              queueClient.deleteMessage(
-                mensagemRecebida.messageId,
-                mensagemRecebida.popReceipt
-              );
-            })
-
-            // // console.log(receivedMessage);
-
-          })
-
-
-        }).catch(e => {
-          console.log(e);
-        })
+      if (name.includes(".json")) {
+        console.log("O arquivo, ", name, " chegou no container (contoso-json)");
       }
-      )
+      else if (name.includes(".xml")) {
+        console.log("O arquivo : ", name, " foi enviado para o container, lendo o arquivo...");
+        axios.get(url, {
+          onDownloadProgress: (progressEvent => {
+            const total = parseFloat(progressEvent.currentTarget.responseHeaders['Content-Length'])
+            const current = progressEvent.currentTarget.response.length
+
+            let percentCompleted = Math.floor(current / total * 100)
+            console.log("Progresso de download da url " + url, " ", percentCompleted, " %");
+          })
+        }).then(async (xml) => {
+          console.log("Iniciando conversão do xml");
+          let json = xml2json(xml.data);
+          console.log("Conversão finalizada");
+          console.log("Criando arquivo JSON local");
+          let { caminho, filename } = criarJSON(name.replace(".xml", ".json"), json);
+          console.log("Iniciando upload o arquivo JSON");
+          await uploadJSON(fs.createReadStream(caminho), filename);
+          console.log("Feito o upload, deletando arquivo local");
+          fs.unlinkSync(caminho);
+        }).catch(e => {
+          console.log("Não foi possível converter");
+          console.log(e);
+        });
+      }
+      await queueClient.deleteMessage(
+        mensagemRecebida.messageId,
+        mensagemRecebida.popReceipt
+      );
     }
+
+    await sleep(1000);
   }
 
+}
 
-  main();
+uploadXML();
+main();
+
